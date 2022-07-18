977ec6c87a3fead456497f0fa0bad635b7176c5b
murat <murat.yarali@outlook.com>
2022-07-18 15:10:52 +0300

    update

--- tasks/docker-hello-world-app/app/hello-world.py
+++ tasks/docker-hello-world-app/app/hello-world.py
@@ -2,9 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def head():
-    return "<h1>Hello World</h1>"
+@app.route('/')
+def index():
+    return 'Hello World!'
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=80)
+app.run(host='0.0.0.0', port=80)
