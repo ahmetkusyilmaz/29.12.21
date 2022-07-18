1f6fa6091afa9673920f034388d4787b224548c3
murat <murat.yarali@outlook.com>
2022-07-18 14:46:33 +0300

    update

--- tasks/docker-hello-world-app/app/hello-world.py
+++ tasks/docker-hello-world-app/app/hello-world.py
@@ -6,17 +6,5 @@ app = Flask(__name__)
 def head():
     return "<h1>Hello World</h1>"
 
-@app.route("/second")
-def second():
-    return "This is the second page"
-
-@app.route("/third/tunc")
-def third():
-    return "This is the subpath of third path"
-
-@app.route("/forth/<string:id>")
-def forth(id):
-    return f'Id of this page is {id}'
-
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
